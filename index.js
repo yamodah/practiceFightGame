011536116bfa695aad69ed71b1b82bfe12fdf7dd
yamodah <yassinedahlek@gmail.com>
2022-04-18 10:48:39 -0400

    builing in gravity for sprites

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ canvas.height = 576;
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.2;
+
 class Sprite {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
+    this.height = 150;
+    this.width = 50;
   }
   draw() {
     ctx.fillStyle = "red";
-    ctx.fillRect(this.position.x, this.position.y, 50, 150);
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
   update() {
-
+    this.draw();
+    this.position.y += this.velocity.y;
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += 0.2;
+    }
   }
 }
 
@@ -29,11 +39,12 @@ const enemy = new Sprite({
   velocity: { x: 0, y: 0 },
 });
 
-player.draw();
-enemy.draw();
-
 function animate() {
   window.requestAnimationFrame(animate);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.width);
+  player.update();
+  enemy.update();
 }
 
 animate();
