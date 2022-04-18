24bdb0530ed868bc63bf10b66458c26468f70b52
yamodah <yassinedahlek@gmail.com>
2022-04-18 11:17:19 -0400

    basic side to side movement added

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ class Sprite {
   update() {
     this.draw();
     this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
       this.velocity.y = 0;
     } else {
@@ -39,12 +40,79 @@ const enemy = new Sprite({
   velocity: { x: 0, y: 0 },
 });
 
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  ArrowLeft: {
+    pressed: false,
+  },
+  ArrowRight: {
+    pressed: false,
+  },
+};
+let lastKey;
 function animate() {
   window.requestAnimationFrame(animate);
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.width);
   player.update();
   enemy.update();
+  player.velocity.x = 0;
+  enemy.velocity.x = 0;
+  if (keys.a.pressed && lastKey === "a") {
+    player.velocity.x = -1;
+  } else if (keys.d.pressed && lastKey === "d") {
+    player.velocity.x = 1;
+  } else if (keys.ArrowLeft.pressed && lastKey === "ArrowLeft") {
+    enemy.velocity.x = -1;
+  } else if (keys.ArrowRight.pressed && lastKey === "ArrowRight") {
+    enemy.velocity.x = 1;
+  }
 }
 
 animate();
+
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      lastKey = "d";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      lastKey = "a";
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = true;
+      lastKey = "ArrowLeft";
+      break;
+    case "ArrowRight":
+      keys.ArrowRight.pressed = true;
+      lastKey = "ArrowRight";
+      break;
+    default:
+  }
+  console.log(event.key);
+});
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = false;
+      break;
+    case "ArrowRight":
+      keys.ArrowRight.pressed = false;
+      break;
+    default:
+  }
+  console.log(event.key);
+});
