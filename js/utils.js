d6a638425312b6f3e29317973bba5051b8192923
yamodah <yassinedahlek@gmail.com>
2022-04-19 12:00:53 -0400

    added utils and imported

---
+++ js/utils.js
@@ -0,0 +1,36 @@
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+      rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
+        rectangle2.position.x &&
+      rectangle1.attackBox.position.x <=
+        rectangle2.position.x + rectangle2.width &&
+      rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+        rectangle2.position.y &&
+      rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    );
+  }
+
+function determineWinner({ player, enemy, timerId }) {
+    clearTimeout(timerId);
+    document.querySelector("#result").style.display = "flex";
+    if (player.health === enemy.health) {
+      document.querySelector("#result").innerHTML = "TIE";
+    } else if (player.health > enemy.health) {
+      document.querySelector("#result").innerHTML = "PLAYER 1 WINS";
+    } else if (player.health < enemy.health) {
+      document.querySelector("#result").innerHTML = "PLAYER 2 WINS";
+    }
+  }
+  
+  let timer = 60;
+  let timerId;
+  function decreaseTimer() {
+    timerId = setTimeout(decreaseTimer, 1000);
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timer").innerHTML = timer;
+    }
+    if (timer === 0) {
+      determineWinner({ player, enemy, timerId });
+    }
+  }
