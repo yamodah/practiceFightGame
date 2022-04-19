9bef47f22d46edeaa611a5f7430ccdf85c62a349
yamodah <yassinedahlek@gmail.com>
2022-04-19 14:39:51 -0400

    jump fully implemented -- removed flash on impact to ground

--- js/classes.js
+++ js/classes.js
@@ -103,6 +103,7 @@ class Fighter extends Sprite {
     this.position.x += this.velocity.x;
     if (this.position.y + this.height + this.velocity.y >= canvas.height - 96) {
       this.velocity.y = 0;
+      this.position.y = 330;
     } else {
       this.velocity.y += gravity;
     }
@@ -119,28 +120,28 @@ class Fighter extends Sprite {
         if (this.image !== this.sprites.idle.image) {
           this.image = this.sprites.idle.image;
           this.framesMax = this.sprites.idle.framesMax;
-          this.framesCurrent = 0
+          this.framesCurrent = 0;
         }
         break;
       case "run":
         if (this.image !== this.sprites.run.image) {
           this.image = this.sprites.run.image;
           this.framesMax = this.sprites.run.framesMax;
-          this.framesCurrent = 0
+          this.framesCurrent = 0;
         }
         break;
       case "jump":
         if (this.image !== this.sprites.jump.image) {
           this.image = this.sprites.jump.image;
           this.framesMax = this.sprites.jump.framesMax;
-          this.framesCurrent = 0
+          this.framesCurrent = 0;
         }
         break;
       case "fall":
         if (this.image !== this.sprites.fall.image) {
           this.image = this.sprites.fall.image;
           this.framesMax = this.sprites.fall.framesMax;
-          this.framesCurrent = 0
+          this.framesCurrent = 0;
         }
         break;
     }
